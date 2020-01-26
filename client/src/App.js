86eeb9c97331e86370e6f18e80a8c2d01f37f9ee
Adarsh Pala <adarsh.pala@hubdoc.com>
2020-01-25 19:28:46 -0500

    Add login and sign in buttons

--- client/src/App.js
+++ client/src/App.js
@@ -23,18 +23,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button>Login</button>
+          <button>Sign Up</button>
           <p>{this.state.apiResponse}</p>
         </header>
       </div>
